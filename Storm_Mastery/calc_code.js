6d61efb9b594ba43af6869aaf795636337766873
qwerx22 <qwerx2202@gmail.com>
2026-05-01 21:09:43 +0200

    update

--- Storm_Mastery/calc_code.js
+++ Storm_Mastery/calc_code.js
@@ -6,6 +6,14 @@ var calc = window.setInterval(function() {
     storm = id("storm").value
     
     cost = ((5 * (next_level ** 2)) + (95 * next_level)) - ((5 * (current_level ** 2)) + (95 * current_level))
+    current_progress = id("progress").value
+    maximum_progress = 100 + 10 * current_level
+    id("max_progress").innerHTML = maximum_progress
+    
+    if (current_progress >= maximum_progress) {
+        current_progress = (maximum_progress - 1)
+        id("progress").value = (maximum_progress - 1)
+    }
     
     if (storm=='tire') {
         id("boost0").innerHTML = current_level - Math.floor(current_level / 100) * 100 +"%, "+ 2 ** Math.floor(1 + current_level / 100) +"x"
@@ -14,22 +22,21 @@ var calc = window.setInterval(function() {
         id("boost0").innerHTML = current_level +"%"
         id("boost1").innerHTML = next_level +"%"
     }
-    
-    id("cost0").innerHTML = cost
+    id("cost0").innerHTML = cost - current_progress
     if (storm=='magnet') {
-        id("cost1").innerHTML = Math.ceil(cost / 35)
-        id("expectedtime").innerHTML = Math.ceil(cost / 35) * (5/12) 
+        id("cost1").innerHTML = Math.ceil((cost - current_progress) / 35)
+        id("expectedtime").innerHTML = Math.ceil((cost - current_progress) / 35) * (5/12)
     } else if (storm=='tire') {
-        id("cost1").innerHTML = Math.ceil(cost / 20)
-        id("expectedtime").innerHTML = Math.ceil(cost / 20) * (5/12)
+        id("cost1").innerHTML = Math.ceil((cost - current_progress) / 20)
+        id("expectedtime").innerHTML = Math.ceil((cost - current_progress) / 20) * (5/12)
     } else if (storm=='GS') {
-        id("cost1").innerHTML = Math.ceil(cost / 35)
-        id("expectedtime").innerHTML = Math.ceil(cost / 35) * (5/12)
+        id("cost1").innerHTML = Math.ceil((cost - current_progress) / 35)
+        id("expectedtime").innerHTML = Math.ceil((cost - current_progress) / 35) * (5/12)
     } else if (storm=='wrench') {
-        id("cost1").innerHTML = Math.ceil(cost / 100)
-        id("expectedtime").innerHTML = Math.ceil(cost / 100) * (5/12)
+        id("cost1").innerHTML = Math.ceil((cost - current_progress) / 100)
+        id("expectedtime").innerHTML = Math.ceil((cost - current_progress) / 100) * (5/12)
     } else if (storm=='beam') {
-        id("cost1").innerHTML = Math.ceil(cost / 20)
-        id("expectedtime").innerHTML = Math.ceil(cost / 20) * (5/12)
+        id("cost1").innerHTML = Math.ceil((cost - current_progress) / 20)
+        id("expectedtime").innerHTML = Math.ceil((cost - current_progress) / 20) * (5/12)
     }
 }, 50)
